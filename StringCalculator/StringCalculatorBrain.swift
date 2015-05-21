a900b79ebf80162c828706ea758ec834790e3174
Guilherme Pacheco <guilherme.f.pacheco@gmail.com>
2015-05-21 17:05:54 -0300

    Add empty string

---
+++ StringCalculator/StringCalculatorBrain.swift
@@ -0,0 +1,15 @@
+//
+//  StringCalculatorBrain.swift
+//  StringCalculator
+//
+//  Created by Guilherme Pacheco on 5/21/15.
+//  Copyright (c) 2015 Guilherme Pacheco. All rights reserved.
+//
+
+struct StringCalculatorBrain {
+
+    func add(string: String) -> Int {
+        return 0
+    }
+
+}
